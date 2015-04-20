974f243cdad4af16d6176a95824bb1153dbe3a61
Bystroushaak <bystrousak@kitakitsune.org>
2015-04-20 14:29:16 +0200

    Added template of the test file.

---
+++ tests/test_uml_templater.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import sys
+
+import pytest
+
+sys.path.append('src/')
+import uml_templater
+
+
+# Variables ===================================================================
+
+
+
+# Functions & classes =========================================================
+def test_uml_templater():
+    pass
